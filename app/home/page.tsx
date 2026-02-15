33fe288f51c4e85e719e2d01f55618ccb3b595a0
Santosh Shet <santosh.vrbr@gmail.com>
2026-02-15 18:25:33 +0530

    linkedin insights tag added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -54,6 +54,28 @@ function Homepage() {
         `}
       </Script>
 
+      {/* LinkedIn Insight Tag */}
+      <Script id="linkedin-insight-partner" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "8760020";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+      <Script id="linkedin-insight-loader" strategy="afterInteractive">
+        {`
+          (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);
+        `}
+      </Script>
+
       <div>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -63,6 +85,17 @@ function Homepage() {
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
+        </noscript>
+
+        {/* LinkedIn Insight Tag (noscript fallback) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8760020&fmt=gif"
+          />
         </noscript>
 
         <HeroBanner />
